443d7597f0409eae467f21250a92246f782f0abf
xuke <741919791@qq.com>
2018-05-30 15:28:00 +0800

    oss整合

---
+++ src/main/java/cn/lfungame/controller/OssController.java
@@ -0,0 +1,29 @@
+package cn.lfungame.controller;
+
+import cn.lfungame.model.OssAK;
+import cn.lfungame.service.OssService;
+import cn.lfungame.util.ResponseMsg;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: xuke
+ * @Date: 2018/5/30 14:32
+ * @Description:
+ */
+@RestController
+@RequestMapping(value = "/oss")
+public class OssController {
+    @Autowired
+    private OssService ossService;
+
+    @PostMapping(value = "/liaowan")
+    Object liaowanBucket() throws Exception {
+        ResponseMsg msg = new ResponseMsg<>();
+        OssAK ak = ossService.getAK();
+        msg.setData(ak);
+        return msg;
+    }
+}
---
+++ src/main/java/cn/lfungame/service/OssService.java
@@ -0,0 +1,76 @@
+package cn.lfungame.service;
+
+import cn.lfungame.model.OssAK;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.auth.sts.AssumeRoleRequest;
+import com.aliyuncs.auth.sts.AssumeRoleResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Auther: xuke
+ * @Date: 2018/5/30 14:33
+ * @Description:
+ */
+@Service
+public class OssService {
+    @Value("${oss.endpoint}")
+    private String endpoint;
+    @Value("${oss.accessKeyId}")
+    private String accessKeyId;
+    @Value("${oss.accessKeySecret}")
+    private String accessKeySecret;
+    // String accessKeySecret = "tyvw5tNmsfRVn0E7NKEcbztSELA2C5";
+    @Value("${oss.roleArn}")
+    private String roleArn;
+    @Value("${oss.roleSessionName}")
+    private String roleSessionName;
+
+    String policy =  "{\n" +
+            "  \"Version\": \"1\",\n" +
+            "  \"Statement\": [\n" +
+            "    {\n" +
+            "      \"Effect\": \"Allow\",\n" +
+            "      \"Action\": [\n" +
+            "        \"oss:*\"\n" +
+            "      ],\n" +
+            "      \"Resource\": [\n" +
+            "        \"acs:oss:*:*:liaowan\",\n" +
+            "        \"acs:oss:*:*:liaowan/\"\n" +
+            "      ],\n" +
+            "      \"Condition\": {}\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}";
+    public OssAK getAK() {
+        OssAK ak = new OssAK();
+        try {
+            // Init ACS Client
+            DefaultProfile.addEndpoint("", "oss-cn-shenzhen", "Sts", endpoint);
+            IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+            DefaultAcsClient client = new DefaultAcsClient(profile);
+            final AssumeRoleRequest request = new AssumeRoleRequest();
+            request.setMethod(MethodType.POST);
+            request.setRoleArn(roleArn);
+            request.setRoleSessionName(roleSessionName);
+            request.setPolicy(policy); // Optional
+            final AssumeRoleResponse response = client.getAcsResponse(request);
+
+            ak.setRequestId(response.getRequestId());
+            ak.setAccessKeyId(response.getCredentials().getAccessKeyId());
+            ak.setAccessKeySecret(response.getCredentials().getAccessKeySecret());
+            ak.setSecurityToken(response.getCredentials().getSecurityToken());
+            ak.setExpiration(response.getCredentials().getExpiration());
+        } catch (ClientException e) {
+            System.out.println("Failed：");
+            System.out.println("Error code: " + e.getErrCode());
+            System.out.println("Error message: " + e.getErrMsg());
+            System.out.println("RequestId: " + e.getRequestId());
+        }
+        return ak;
+    }
+}
